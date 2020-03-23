42cadeadf41e810de9375cf2d1f44ba9b0ea24af
Iiro Sinisalo <iiro.sinisalo@tuni.fi>
2020-03-23 13:09:14 +0000

    Something

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,6 @@
 const passport = require("passport");
 
 module.exports = {
-    // Why has this been added? It's practically the same as ensureAuthenticated.
     passUser(req, res, next) { //TODO: Rename this to something else. Its used to pass user if token present atm
         passport.authenticate("jwt", {
             session: false
